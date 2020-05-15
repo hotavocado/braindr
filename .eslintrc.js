02a867f11d542616c6e755d76daf0de1a13e1d87
Mike Xiao <mikemxxiao@gmail.com>
2020-05-14 21:17:30 -0400

    connect to firebase and deploy test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,37 @@
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
-  env: {
-    browser: true,
-  },
-  extends: 'airbnb-base',
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  // check if imports actually resolve
-  'settings': {
-    'import/resolver': {
-      'webpack': {
-        'config': 'build/webpack.base.conf.js'
-      }
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
     }
-  },
-  // add your custom rules here
-  'rules': {
-    // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      'js': 'never',
-      'vue': 'never'
-    }],
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      'optionalDependencies': ['test/unit/index.js']
-    }],
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
-}
+};
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,14 +2,17 @@ import firebase from 'firebase';
 
 // copy/paste this from your firebase console
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-};
+  apiKey: "AIzaSyAYM1gtos8UGNmu2oc5AMpXEFsohiH9Cqs",
+  authDomain: "hbnbraindr.firebaseapp.com",
+  databaseURL: "https://hbnbraindr.firebaseio.com",
+  projectId: "hbnbraindr",
+  storageBucket: "hbnbraindr.appspot.com",
+  messagingSenderId: "229562143310",
+  appId: "1:229562143310:web:91837e8b12ffb23e0ce91f",
+  measurementId: "G-X2NN6F7556"
+};;
 
 firebase.initializeApp(config);
 
 export const db = firebase.database();
+
